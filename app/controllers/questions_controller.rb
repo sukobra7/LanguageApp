615e48ba635079b7d36f4d84f7abb2b681cdb1cb
suko0602 <54353147+suko0602@users.noreply.github.com>
2020-11-10 09:36:28 +0900

    - question作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,8 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :update, :edit, :destroy]
   
   def index
-    @questions = Quation.all
+    @questions = Question.all
   end
 
   def new
@@ -14,4 +15,34 @@ class QuestionsController < ApplicationController
   def show
   end
 
+  def create
+    @question = Question.create(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def question_params
+    params.require(:question).permit(:title, :text)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
 end
+
