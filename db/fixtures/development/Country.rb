9997d266e2d0c37febfdd8605c00a0fa6e140162
suko0602 <54353147+suko0602@users.noreply.github.com>
2020-11-15 16:39:30 +0900

    - seedファイル作成

---
+++ db/fixtures/development/Country.rb
@@ -0,0 +1,65 @@
+Country.seed do |s|
+  s.id = 1
+  s.name = America
+end
+
+Country.seed do |s|
+  s.id = 2
+  s.name = Canada
+end
+
+Country.seed do |s|
+  s.id = 3
+  s.name = Japan
+end
+
+Country.seed do |s|
+  s.id = 4
+  s.name = China
+end
+
+Country.seed do |s|
+  s.id = 5
+  s.name = Korea
+end
+
+Country.seed do |s|
+  s.id = 6
+  s.name = Spain
+end
+
+Country.seed do |s|
+  s.id = 7
+  s.name = Brazil
+end
+
+Country.seed do |s|
+  s.id = 8
+  s.name = France
+end
+
+Country.seed do |s|
+  s.id = 9
+  s.name = Germany
+end
+
+Country.seed do |s|
+  s.id = 10
+  s.name = Turkish
+end
+
+Country.seed do |s|
+  s.id = 11
+  s.name = Vietnam
+end
+
+Country.seed do |s|
+  s.id = 12
+  s.name = Thailand
+end
+
+Country.seed do |s|
+  s.id = 13
+  s.name = Australia
+end
+
---
+++ db/fixtures/development/Language.rb
@@ -0,0 +1,51 @@
+Language.seed do |s|
+  s.id = 1
+  s.name = English
+end
+
+Language.seed do |s|
+  s.id = 2
+  s.name = Japanese
+end
+
+Language.seed do |s|
+  s.id = 3
+  s.name = Korean
+end
+
+Language.seed do |s|
+  s.id = 4
+  s.name = Chinese
+end
+
+Language.seed do |s|
+  s.id = 5
+  s.name = Spanish
+end
+
+Language.seed do |s|
+  s.id = 6
+  s.name = Portugues
+end
+
+Language.seed do |s|
+  s.id = 7
+  s.name = Germany
+end
+
+Language.seed do |s|
+  s.id = 8
+  s.name = French
+end
+
+Language.seed do |s|
+  s.id = 9
+  s.name = Turkish
+end
+
+Language.seed do |s|
+  s.id = 10
+  s.name = Takalog
+end
+
+
